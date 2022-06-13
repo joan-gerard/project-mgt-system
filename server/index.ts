83c6954d043fb15d4301298f152b9a85e3647ed8
Joan Gerard <joan.gerard@outlook.com>
2022-06-13 18:05:12 +0200

    connect to mongodb

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,24 @@
 import express from "express";
+import colors from "colors";
+import cors from "cors";
+require('dotenv').config();
 const port: string | number = process.env.PORT || 5000;
 import { graphqlHTTP } from "express-graphql";
-import schema from './schema/schema';
-require("dotenv").config();
-
+import schema from "./schema/schema";
+import { connectDB } from "./config/db";
 const app = express();
 
-app.use('/graphql', graphqlHTTP({
+// connect tp DB
+connectDB();
+
+app.use(cors())
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema,
-    graphiql: process.env.NODE_ENV === 'development'
-}))
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
