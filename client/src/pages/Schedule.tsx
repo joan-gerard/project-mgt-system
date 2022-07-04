1d827881efac9c9c7031e97495e3e7725fd04322
Joan Gerard <joan.gerard@outlook.com>
2022-07-04 19:53:02 +0200

    display database tasks on schedule chart

--- client/src/pages/Schedule.tsx
+++ client/src/pages/Schedule.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import { FrappeGantt } from "frappe-gantt-react";
 
 import { useParams } from "react-router-dom";
@@ -6,14 +6,31 @@ import { useQuery } from "@apollo/client";
 import { GET_PROJECT } from "../queries/projectQueries";
 import Spinner from "../components/Spinner";
 import { sampleTasks } from "../sampleTasks";
+import { GET_TASKS } from "../queries/taskQueries";
 
 const Schedule = () => {
+  const [tasks, setTasks] = useState<any>([{}]);
+
   const { id } = useParams();
   const { loading, error, data } = useQuery(GET_PROJECT, {
     variables: { id },
   });
 
-  const [tasks, setTasks] = useState<any>(sampleTasks || {});
+  const {
+    loading: taskLoading,
+    error: taskError,
+    data: taskData,
+  } = useQuery(GET_TASKS);
+
+  useEffect(() => {
+    if (!loading && !error && !taskLoading && !taskError) {
+      const projectTasks = taskData.tasks.filter(
+        (task: any) => task.projectId === id
+      );
+      console.log("projectTasks", projectTasks);
+      setTasks(projectTasks);
+    }
+  }, [loading, error, taskLoading, taskError, taskData]);
 
   const [taskName, setTaskName] = useState("");
   const [taskStart, setTaskStart] = useState("");
@@ -34,10 +51,14 @@ const Schedule = () => {
     };
 
     setTasks([...tasks, newTask]);
+    
   };
 
   if (loading) return <Spinner />;
   if (error) return <p>Something Went Wrong</p>;
+
+  console.log("the taskData: ", taskData);
+  console.log("the data: ", tasks);
 
   return (
     <>
@@ -87,18 +108,22 @@ const Schedule = () => {
             </div>
             <button type="submit">Submit</button>
           </form>
-          <FrappeGantt
-            tasks={tasks}
-            // viewMode={this.state.mode}
-            onClick={(task) => console.log("log 1", task, "click")}
-            onDateChange={(task, start, end) =>
-              console.log("log 2: ", task, start, end, "date")
-            }
-            onProgressChange={(task, progress) =>
-              console.log("log 3: ", task, progress, "progress")
-            }
-            onTasksChange={(tasks) => console.log("log 4:", tasks, "tasks")}
-          />
+          <>
+            {!loading && !error && !taskLoading && !taskError && taskData && (
+              <FrappeGantt
+                tasks={tasks}
+                // viewMode={this.state.mode}
+                onClick={(task) => console.log("log 1", task, "click")}
+                onDateChange={(task, start, end) =>
+                  console.log("log 2: ", task, start, end, "date")
+                }
+                onProgressChange={(task, progress) =>
+                  console.log("log 3: ", task, progress, "progress")
+                }
+                onTasksChange={(tasks) => console.log("log 4:", tasks, "tasks")}
+              />
+            )}
+          </>
         </div>
       )}
     </>
